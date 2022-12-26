a98fc8f37804f61445c4b61c5375d90b04de4ecd
Emre <112270755+emrrekus@users.noreply.github.com>
2022-12-26 18:59:08 +0300

    floor controller

---
+++ Proje2/Assets/GameFolders/Script/Concrets/Controllers/FloorController.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Proje2.Controllers
+{
+    public class FloorController : MonoBehaviour
+    {
+        private Material _material;
+        [Range(0.5f,2f)]
+        [SerializeField] private float _moveSpeed = 0.5f;
+        private void Awake()
+        {
+            _material = GetComponentInChildren<MeshRenderer>().material;
+        }
+
+        private void Update()
+        {
+            _material.mainTextureOffset += Vector2.down * Time.deltaTime * _moveSpeed;
+        }
+    }
+    
+}
+
