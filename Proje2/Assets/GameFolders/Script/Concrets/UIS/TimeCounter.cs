2a231880d1e5f07bb5487d3015bebe2a40446a8c
Emre <112270755+emrrekus@users.noreply.github.com>
2022-12-27 16:23:17 +0300

    TimeCounter

---
+++ Proje2/Assets/GameFolders/Script/Concrets/UIS/TimeCounter.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace Proje2.UIS
+{
+    public class TimeCounter : MonoBehaviour
+    {
+        private TMP_Text _text;
+        private float _currentTime;
+
+        private void Awake()
+        {
+            _text = GetComponent<TMP_Text>();
+        }
+
+        private void Update()
+        {
+            _currentTime += Time.deltaTime;
+            _text.text = _currentTime.ToString("0");
+        }
+    } 
+}
+
